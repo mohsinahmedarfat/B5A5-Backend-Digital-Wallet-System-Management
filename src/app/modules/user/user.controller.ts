166eae756fd75b5e94baeb80a77b8659387735cd
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-07-30 13:34:12 +0600

    feat: user interface, model, service, controller, route and create user

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await UserServices.createUser(req.body);
+
+    res.status(httpStatus.CREATED).json({
+      success: true,
+      message: "User created successfully!",
+      user,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: `Something went wrong!! ${error.message}`,
+      error,
+    });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
