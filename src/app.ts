df3147cc5b6e938a598b59630a6621dcc1ec2bb5
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-08-22 19:23:08 +0600

    add: cors policy

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,29 @@ import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import passport from "passport";
-import expressSession from "express-session"
-import "./app/config/passport"
+import expressSession from "express-session";
+import "./app/config/passport";
 import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
-app.use(cookieParser())
-app.use(expressSession({
-  secret: "strong secret", // Replace with a strong secret
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: "strong secret", // Replace with a strong secret
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", router);
 
