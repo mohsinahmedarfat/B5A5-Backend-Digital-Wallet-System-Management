24e0e79b5ceae143794a576d2cec0eeace8301bd
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-08-22 19:33:49 +0600

    fix: tsc build

--- dist/app.js
+++ dist/app.js
@@ -17,12 +17,15 @@ app.use((0, cookie_parser_1.default)());
 app.use((0, express_session_1.default)({
     secret: "strong secret", // Replace with a strong secret
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
 }));
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use("/api/v1", routes_1.router);
 app.get("/", (req, res) => {
     res.send("Welcome to Digital Wallet System Management.");
--- dist/app/utils/setCookies.js
+++ dist/app/utils/setCookies.js
@@ -4,13 +4,13 @@ const setCookie = (res, tokenInfo) => {
     if (tokenInfo.accessToken) {
         res.cookie("accessToken", tokenInfo.accessToken, {
             httpOnly: true,
-            secure: false,
+            secure: true,
         });
     }
     if (tokenInfo.refreshToken) {
         res.cookie("refreshToken", tokenInfo.refreshToken, {
             httpOnly: true,
-            secure: false,
+            secure: true,
         });
     }
 };
