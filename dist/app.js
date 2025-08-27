8d7b9bccc83942f3c34644e8cf169a263c822f5d
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-08-27 21:50:33 +0600

    cors policy updated

--- dist/app.js
+++ dist/app.js
@@ -23,7 +23,10 @@ app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: [
+        "https://digital-wallet-system-management.vercel.app",
+        "http://localhost:5173",
+    ],
     credentials: true,
 }));
 app.use("/api/v1", routes_1.router);
--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,10 @@ app.use(passport.session());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: [
+      "https://digital-wallet-system-management.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
